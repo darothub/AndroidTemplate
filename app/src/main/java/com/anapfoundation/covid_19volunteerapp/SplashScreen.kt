6fe833e449c8240025475a26ce399ab314c262d4
darot <darotudeen@gmail.com>
2020-04-29 20:17:33 +0100

    added logo

---
+++ app/src/main/java/com/anapfoundation/covid_19volunteerapp/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.anapfoundation.covid_19volunteerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
