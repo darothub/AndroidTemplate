75ff4772c717dbb7326befb31c953b92f0e130a1
darot <darotudeen@gmail.com>
2020-04-30 02:11:49 +0100

    added home screen

--- app/src/main/java/com/anapfoundation/covid_19volunteerapp/ui/HomeFragment.kt
+++ app/src/main/java/com/anapfoundation/covid_19volunteerapp/ui/HomeFragment.kt
@@ -7,17 +7,25 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.anapfoundation.covid_19volunteerapp.R
+import com.anapfoundation.covid_19volunteerapp.utils.extensions.getName
+
 
 /**
  * A simple [Fragment] subclass.
  */
+
 class HomeFragment : Fragment() {
 
+    val title:String by lazy {
+        getName()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        requireActivity().window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
