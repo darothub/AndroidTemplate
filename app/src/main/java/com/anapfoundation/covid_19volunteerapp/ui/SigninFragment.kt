af4e3f5375abcccb92e19b9616f7fc01613cc4da
darot <darotudeen@gmail.com>
2020-04-30 21:31:52 +0100

    Added text field and clickable signup text

--- app/src/main/java/com/anapfoundation/covid_19volunteerapp/ui/SigninFragment.kt
+++ app/src/main/java/com/anapfoundation/covid_19volunteerapp/ui/SigninFragment.kt
@@ -1,9 +1,12 @@
 package com.anapfoundation.covid_19volunteerapp.ui
 
+import android.graphics.Paint
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,8 +15,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 
 import com.anapfoundation.covid_19volunteerapp.R
-import com.anapfoundation.covid_19volunteerapp.utils.extensions.getName
-import com.anapfoundation.covid_19volunteerapp.utils.extensions.localized
+import com.anapfoundation.covid_19volunteerapp.utils.extensions.*
 import kotlinx.android.synthetic.main.fragment_signin.*
 
 /**
@@ -28,26 +30,36 @@ class SigninFragment : Fragment() {
         requireContext().localized(R.string.signup_link)
     }
     val spannableString:SpannableString by lazy {
-        SpannableString(signupText)
+        signupText.setAsSpannable()
     }
-    val color:ForegroundColorSpan by lazy {
-        ForegroundColorSpan(resources.getColor(R.color.colorPrimary))
+    val color:Int by lazy {
+        resources.getColor(R.color.colorPrimary)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        showStatusBar()
         return inflater.inflate(R.layout.fragment_signin, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-
-        spannableString.setSpan(color, 23, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        Toast.makeText(context, signupText, Toast.LENGTH_SHORT).show()
-
+        spannableString.enableClickOnSubstring(23, 30){
+            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+        spannableString.setColorToSubstring(color, 23, 30)
+        spannableString.removeUnderLine(23, 30)
         signup_link.text = spannableString
+        signup_link.movementMethod = LinkMovementMethod.getInstance()
+
+//        signup_link.paintFlags = signup_link.paintFlags and Paint.UNDERLINE_TEXT_FLAG.inv()
     }
+
+
+
 }
--- app/src/main/java/com/anapfoundation/covid_19volunteerapp/utils/extensions/FragmentExtensions.kt
+++ app/src/main/java/com/anapfoundation/covid_19volunteerapp/utils/extensions/FragmentExtensions.kt
@@ -1,6 +1,7 @@
 package com.anapfoundation.covid_19volunteerapp.utils.extensions
 
 import android.content.Context
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
@@ -8,4 +9,8 @@ import com.anapfoundation.covid_19volunteerapp.R
 
 fun Fragment.getName():String{
     return this::class.qualifiedName!!
+}
+
+fun Fragment.showStatusBar(){
+    requireActivity().window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 }
