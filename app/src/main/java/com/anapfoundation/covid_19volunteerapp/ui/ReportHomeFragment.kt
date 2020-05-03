ac9e284a4b5581cdb4e0ba61ee009138e27145ed
darot <darotudeen@gmail.com>
2020-05-03 22:51:26 +0100

    added report home screen

--- app/src/main/java/com/anapfoundation/covid_19volunteerapp/ui/ReportHomeFragment.kt
+++ app/src/main/java/com/anapfoundation/covid_19volunteerapp/ui/ReportHomeFragment.kt
@@ -7,6 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.anapfoundation.covid_19volunteerapp.R
+import com.anapfoundation.covid_19volunteerapp.utils.extensions.toast
+import com.utsman.recycling.setupAdapter
+import kotlinx.android.synthetic.main.fragment_report_home.*
+import kotlinx.android.synthetic.main.report_item.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +23,22 @@ class ReportHomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_report_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val myList = listOf<String>("Hello", "Nigeria", "Ghana", "Algeria", "Panama", "Antarctica")
+        recyclerView.setupAdapter<String>(R.layout.report_item){adapter, context, list ->
+            bind { itemView, position, item ->
+                itemView.reportTitle.text = item
+                itemView.setOnClickListener {
+                    requireActivity().toast("Pos $position")
+                }
+            }
+
+            submitList(myList)
+        }
     }
 
 }
