c6472dfe4f53117622d7e4d2ec17be5982ec6b3e
darot <darotudeen@gmail.com>
2020-06-29 06:41:58 +0100

    changed no-report placeholder

--- app/src/main/java/com/anapfoundation/volunteerapp/network/auth/AuthRequestInterface.kt
+++ app/src/main/java/com/anapfoundation/volunteerapp/network/auth/AuthRequestInterface.kt
@@ -10,36 +10,47 @@ import retrofit2.Call
 
 interface AuthRequestInterface {
 
-    fun addReport(topic:String,
-                  rating:String,
-                  story:String,
-                  state:String,
-                  mediaURL:String?="",
-                  localGovernment:String?,
-                  district:String?,
-                  town:String?,
-                  zone:String?,
-                  suggestion:String?="",
-                  header:String): Call<AddReportResponse>
+    fun addReport(
+        topic: String,
+        rating: String,
+        story: String,
+        state: String,
+        mediaURL: String? = "",
+        localGovernment: String?,
+        district: String?,
+        town: String?,
+        zone: String?,
+        suggestion: String? = "",
+        header: String
+    ): Call<AddReportResponse>
 
     fun getTopic(header: String): Call<TopicResponse>
 
-    fun getRating(topicID:String, header: String): Call<TopicResponse>
+    fun getRating(topicID: String, header: String): Call<TopicResponse>
 
-    fun getProfileData(header: String):Call<ProfileData>
+    fun getProfileData(header: String): Call<ProfileData>
 
-    fun getReports(header: String, first:Long?, after:Long?):Call<Reports>
+    fun getReports(header: String, first: Long?, after: Long?): Call<Reports>
 
     fun updateProfile(
-        firstName:String, lastName:String, email: String, phone: String,
-        houseNumber:String?=null,  state:String?, street: String?, localGovernment: String?, zone:String?, profileImageUrl:String?=null, header:String
-    ):Call<ProfileData>
+        firstName: String,
+        lastName: String,
+        email: String,
+        phone: String,
+        houseNumber: String? = null,
+        state: String?,
+        street: String?,
+        localGovernment: String?,
+        zone: String?,
+        profileImageUrl: String? = null,
+        header: String
+    ): Call<ProfileData>
 
-    fun resetPassword(newPassword:String, token: String):Call<UserResponse>
+    fun resetPassword(newPassword: String, token: String): Call<UserResponse>
 
-    fun approveReport(id:String, header: String):Call<DefaultResponse>
-    
-    fun dismissReport(id:String, header: String):Call<DefaultResponse>
+    fun approveReport(id: String, header: String): Call<DefaultResponse>
+
+    fun dismissReport(id: String, header: String): Call<DefaultResponse>
 
 //    fun getUnapprovedReports(header: String, first:Long?, after:Long?):Call<Reports>
 
